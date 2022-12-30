3fe8e484c94e5600e1e61d809c3785a76f9e18e3
leooc1 <leonardoocvrb@gmail.com>
2022-12-30 18:42:55 -0300

    atualização da nossa ideia pro newto ver

--- renomear/index.js
+++ renomear/index.js
@@ -1,11 +1,16 @@
 function clicar(){
     let a = window.document.getElementById("sanduiche");
+    let b = window.document.querySelector("div.menu-sanduiche");
     if(a.style.transform == "rotate(90deg)"){
         a.style.transform = "rotate(0deg)";
+        a.style.right = "10px";
+        b.style.display = "none";
     }
     else{
         a.style.transform = "rotate(90deg)";
-    a.style.transitionDuration = ".4s"
+        a.style.right = "330px";
+        a.style.transitionDuration = ".4s"
+        b.style.display= "flex"
     }
     
 
